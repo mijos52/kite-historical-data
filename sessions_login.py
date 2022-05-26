b0000c8fb6b4932a73ba58ddf734703c85892da7
mijos52 <styleethnic1@gmail.com>
2022-05-26 10:45:29 +0530

    candle data success

--- sessions_login.py
+++ sessions_login.py
@@ -1,10 +1,12 @@
-from dataclasses import dataclass
 import requests 
 from pprint import pprint
 from config import*
-import json
 
-"""logs into kite zerodha using sessions from requests lib"""
+"""logs into kite zerodha using sessions from requests lib
+2 login urls are used for authentication and api url to access the data
+login urls use the same header but api use different header with all necessary tokens which acts as api_keys
+
+"""
 
 url = 'https://kite.zerodha.com/api/login'
 r = requests.get(url)
@@ -40,11 +42,6 @@ request_id = request_data['request_id']
 
 enctoken = 'enctoken'+' '+request_id
 
-print(request_id)
-print(result.cookies)
-
-
-
 # two-factor authentication
 twofa_url = 'https://kite.zerodha.com/api/twofa'
 pay_load = {
@@ -59,86 +56,48 @@ pay_load = {
 # post request for two-factor authentication
 result = session.post(twofa_url,headers=main_header,cookies=cookies,data =pay_load )
 special_cookie = result.cookies
-print(special_cookie)
 
-public_token = ''
+# values for all tokens
+kf_session =result.cookies['kf_session']
+public_token =special_cookie['public_token']
+private_token = special_cookie['enctoken']
 
-
-#set api preference
-
-
-#headers
-
-header={
-
-'authority': 'kite.zerodha.com',
-'method': 'GET',
-'path': '/api/chart/preferences?user_id=AX2602&type=tradingview',
-'scheme': 'https',
-'accept': 'application/json, text/plain, */*',
-'accept-encoding': 'gzip, deflate, br',
-'accept-language': 'en,es;q=0.9',
-'cookie':str(special_cookie),
-'referer': 'https://kite.zerodha.com/chart/web/tvc/NSE/BSE/5013761',
-'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="101", "Google Chrome";v="101"',
-'sec-ch-ua-mobile':'?0',
-'sec-ch-ua-platform':'"Windows"',
-'sec-fetch-dest': 'empty',
-'sec-fetch-mode': 'cors',
-'sec-fetch-site': 'same-origin',
-'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.67 Safari/537.36',
-# 'x-csrftoken': 'aBN5Ff389hgyrsXN52XtFKCRK5enkMpe',
-'x-kite-userid': 'AX2602',
-'x-kite-version': '2.9.11'
-
-}
-
-
-
-pay_load = {
-
-'user_id': 'AX2602',
-'type': 'tradingview'
-}
-
-response = session.get('https://kite.zerodha.com/api/chart/preferences?user_id=AX2602&type=tradingview',cookies=special_cookie,headers=header)
-print(response.text)
-
- 
-""" data requests """
+""" data requests code for kite historical data api  """
 
 headers ={
 
-
 'authority': 'kite.zerodha.com',
 'method': 'GET',
-'path': '/oms/instruments/historical/987393/day?user_id=AX2602&oi=1&from=2021-11-22&to=2022-05-24',
+'path': '/oms/instruments/historical/189185/minute?user_id=AX2602&oi=1&from=2022-05-11&to=2022-05-26',
 'scheme': 'https',
-'accept': 'application/json, text/plain, */*',
+'accept': '*/*',
 'accept-encoding': 'gzip, deflate, br',
 'accept-language': 'en,es;q=0.9',
-'authorization': enctoken,
-'referer': 'https://kite.zerodha.com/chart/ext/tvc/NSE/VARROC/987393',
+'authorization': f'enctoken {private_token}',
+'cookie': f'kf_session={kf_session}; user_id=AX2602; public_token={public_token}; enctoken={private_token}',
+'referer': 'https://kite.zerodha.com/static/build/chart.html?v=2.9.11',
 'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="101", "Google Chrome";v="101"',
 'sec-ch-ua-mobile': '?0',
-'sec-ch-ua-platform': "Windows",
+'sec-ch-ua-platform': '"Windows"',
 'sec-fetch-dest': 'empty',
 'sec-fetch-mode': 'cors',
 'sec-fetch-site': 'same-origin',
-'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.67 Safari/537.36'
-
+'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.67 Safari/537.36'
  }
 
-
+# url inputs
+from_date ='2015-05-25'
+to_date = '2015-05-26'
 
 pay_load = {
+
 'user_id': 'AX2602',
 'oi': '1',
-'from': '2021-11-22',
-'to': '2022-05-24'
-}
+'from': from_date,
+'to': to_date
+ }
 
-response = session.get('https://kite.zerodha.com/oms/instruments/historical/987393/day?user_id=AX2602&oi=1&from=2021-11-22&to=2022-05-24',data=pay_load,headers=headers,cookies=special_cookie,)
+response = session.get(f'https://kite.zerodha.com/oms/instruments/historical/189185/15minute?user_id=AX2602&oi=1&from={from_date}&to={to_date}',headers=headers,cookies=special_cookie,data=pay_load)
 print(response.text)
 
 
