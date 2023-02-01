b67b0d4a43ce6d66fc0cc637bf25cc923c806400
Manu <92246504+mijos52@users.noreply.github.com>
2023-02-01 11:32:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-USER_NAME = 'AX2602'
-PASSWORD = '2mjv6d.6m.1996y.#'
-PIN_NUMBER = '199696'
+USER_NAME = 'user_name'
+PASSWORD = 'pass_word'
+PIN_NUMBER = 'pin_number'
 KITE_LOGIN_URL = 'https://kite.zerodha.com/api/login'
 KITE_TWO_FACTOR_LOGIN_URL ='https://kite.zerodha.com/api/twofa'
 HTTP_HEADER =  {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.67 Safari/537.36'}
